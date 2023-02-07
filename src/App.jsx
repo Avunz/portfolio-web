32f14bc93ec4b97cb8bf2eaec17dac988a489d0a
Avunz <felix@zuluboom.net>
2023-02-07 00:10:28 -0500

    done for the night

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function App() {  
 
   return (
     <div className="App">
-            
+      <div className="bigjim">
+        Hi, my name is Félix.
+      </div>
     </div>
   )
 }
